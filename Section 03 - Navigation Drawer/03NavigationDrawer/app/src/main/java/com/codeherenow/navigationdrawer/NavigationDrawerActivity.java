1f9cda5cd42fc744f1f16967afdd4fbfcf4c5ae4
Ragunath Jawahar <rj@mobsandgeeks.com>
2015-02-11 10:36:23 +0530

    [WC] Import.

---
+++ Section 03 - Navigation Drawer/03NavigationDrawer/app/src/main/java/com/codeherenow/navigationdrawer/NavigationDrawerActivity.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 Code Here Now
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codeherenow.navigationdrawer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * @author Ragunath Jawahar <www.codeherenow.com>
+ */
+public class NavigationDrawerActivity extends Activity {
+
+    private ListView mDrawerListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+
+        // UI References
+        mDrawerListView = (ListView) findViewById(R.id.drawerListView);
+
+        // Initialize the ListView
+        ArrayAdapter<String> websitesAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.websites));
+        mDrawerListView.setAdapter(websitesAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
